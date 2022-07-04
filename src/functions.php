2a747ef5b1e5e29d5b50f14f75b2397d0ea9a78a
LordDeveloper <losrkzpp@hi2.in>
2022-07-04 14:53:01 +0430

    Improved

--- src/functions.php
+++ src/functions.php
@@ -5,6 +5,13 @@ namespace Medoo;
 use Amp\Promise;
 use function Amp\call;
 
+/**
+ * Connecting to specified database
+ *
+ * @param $driver
+ * @param array $options
+ * @return Promise<Database>
+ */
 function connect($driver, array $options): Promise
 {
     return call(function () use ($driver, $options) {
