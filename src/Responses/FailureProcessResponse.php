14794ca767c875b712c900c69498afc10f475c16
LordDeveloper <losrkzpp@hi2.in>
2022-07-04 16:37:30 +0430

    [final] Improved

--- src/Responses/FailureProcessResponse.php
+++ src/Responses/FailureProcessResponse.php
@@ -2,52 +2,31 @@
 
 namespace Medoo\Responses;
 
-use Exception;
-use ReflectionProperty;
 use Throwable;
 
 class FailureProcessResponse implements Response
 {
-    private string $class;
-
-    private array $props = [];
+    private Throwable $exception;
 
     public function __construct(Throwable $e)
     {
-        $this->class = get_class($e);
-
-        $this->props = [
-            'code' => $e->getCode(),
-            'line' => $e->getLine(),
-            'message' => $e->getMessage(),
-            'trace' => $e->getTrace(),
-        ];
+        $this->exception = $e;
     }
 
     public function throw()
     {
-        $throwable = new $this->class();
-
-        foreach ($this->props as $prop => $value) {
-            $reflection = new ReflectionProperty(Exception::class, $prop);
-            $reflection->setAccessible(true);
-            $reflection->setValue($throwable, $value);
-        }
-
-        throw $throwable;
+        throw $this->exception;
     }
 
     public function __serialize()
     {
         return [
-            'class' => $this->class,
-            'props' => $this->props
+            'exception' => $this->exception,
         ];
     }
 
     public function __unserialize(array $data)
     {
-        $this->class = $data['class'];
-        $this->props = $data['props'];
+        $this->exception = $data['exception'];
     }
 }
